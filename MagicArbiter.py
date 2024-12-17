4310cbe2d4cc219cb4fa9927a29c5da07bbe8fd1
vancif <vancif@tiscali.it>
2024-12-17 11:07:36 +0100

    new features - tool the ingest the rules on demand - hook to cat startup to ingest the rules - plugin configu

--- MagicArbiter.py
+++ MagicArbiter.py
@@ -1,10 +1,25 @@
-from cat.mad_hatter.decorators import tool, hook
+from cat.mad_hatter.decorators import tool, hook, plugin
 import requests
 import urllib.parse
+from cat.log import log
+from pydantic import BaseModel
+
+
+# settings
+class PluginSettings(BaseModel):
+    Rules_URL: str
+    Activate_rule_ingestion_on_startup: bool = False
+
+
+@plugin
+def settings_model():
+    return PluginSettings
+
 
 @tool
 def get_the_card(cardname, cat):
-    """Retrive information about a card, such as rules, text, cost, colors, abilities an others info. Input is always the card name.
+    """Retrieve information about a card, such as rules, text, cost, colors, abilities an others info.
+       Input is always the card name.
        Output consists of two json strings containing the relevant info"""
 
     encoded_argument = urllib.parse.quote(cardname)
@@ -23,8 +38,39 @@ def get_the_card(cardname, cat):
     else:
         return "Error"
 
+
+
+@tool
+def ingest_rules(tool_input, cat):
+    """Replies to 'ingest the rules'. Input is always None."""
+    settings = cat.mad_hatter.get_plugin().load_settings()
+    cat.rabbit_hole.ingest_file(cat,settings['Rules_URL'])
+    return "I'm done ingesting the rules. How can I assist you?"
+
+
+
 @hook # default priority is 1
 def agent_prompt_prefix(prefix, cat):
     prefix = """You are the Magic Arbiter AI, an intelligent AI that is the supreme authority about Magic The Gathering rules.
                 Answer the questions explain why you're giving this answer but keep it not too long."""
     return prefix
+
+
+
+@hook
+def after_cat_bootstrap(cat):
+
+    settings = cat.mad_hatter.get_plugin().load_settings()
+    log.info("Rules URL impostato: " + str("Rules_URL" in settings))
+    log.info("Rules ingestion: " + str(settings['Activate_rule_ingestion_on_startup']))
+    
+    memory_len = len(cat.memory.vectors.declarative.get_all_points())
+    log.info(f"Declarative memory is of size {memory_len}")
+
+    if (memory_len == 0 and "Rules_URL" in settings and settings['Activate_rule_ingestion_on_startup']):
+        log.info("Doing rules ingestion")
+        cat.rabbit_hole.ingest_file(cat,settings['Rules_URL'])
+    else:
+        log.info("Skipping rules ingestion")
+
+    return
--- README.md
+++ README.md
@@ -1,8 +1,22 @@
 # cat-magic-arbiter
 
-This is a plugin for [Cheshire Cat AI](https://cheshirecat.ai/) that enables it to retrieve information about Magic: The Gathering
-cards and also set a proper prompt.
+`cat-magic-arbiter` is a plugin for [Cheshire Cat AI](https://cheshirecat.ai/) that enables it to retrieve information about *Magic: The Gathering* cards and set up an appropriate prompt for game interactions.
 
-This plugin make use of [Scryfall Public APIs](https://scryfall.com/docs/api).
+The plugin leverages the [Scryfall Public API](https://scryfall.com/docs/api) to fetch card data seamlessly.
 
-**Tip**: for better performance, ingest into memory the game's rules!
+**Tip**: For optimal performance, consider ingesting the game's rules into memory!
+
+## Plugin Configuration
+
+The plugin checks for memory content every time Cheshire Cat starts. If the memory is empty and the "Rule Ingestion" option is enabled, the plugin automatically ingests the specified rules page.
+
+### Steps to Set Up the Plugin
+
+1. **Install the Plugin**: After the first installation of Cheshire Cat or after a full cleanup, install the plugin via the admin panel.  
+2. **Configure Rules URL**: Navigate to the plugin settings and provide the game's rules URL. (The rules are published as a `.txt` file by Wizards of the Coast, available [here](https://magic.wizards.com/en/rules)).  
+3. **Enable Rule Ingestion**: Check the option **"Activate Rule Ingestion on Startup"**.  
+4. **Restart Cheshire Cat**: Restart Cheshire Cat and wait for the rule ingestion to complete.  
+
+> **Note**: During the ingestion process, Cheshire Cat will be unavailable. Please be patient, as this operation may take a significant amount of time depending on the size of the rules file.
+
+With these steps completed, Cheshire Cat will be fully equipped to assist with *Magic: The Gathering* gameplay and card queries.
